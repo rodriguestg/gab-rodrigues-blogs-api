2621ac4226c4d998cfe9cef48d5b0d157f00ac4b
rodriguestg <grs.ibr@gmail.com>
2022-11-07 18:46:15 -0300

    req 2

--- src/models/User.js
+++ src/models/User.js
@@ -1,12 +1,16 @@
-const UsersModel = (sequelize, DataTypes) => {
+const User = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     display_name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
     image: DataTypes.STRING,
+  },
+  {
+    tableName: 'users',
+    underscored: false,
   });
 
   return User;
 };
 
-module.exports = UsersModel;
+module.exports = User;
