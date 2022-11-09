a3c5a4313e055ec3af2b27020826f3807379c2e9
rodriguestg <grs.ibr@gmail.com>
2022-11-09 05:16:04 -0300

    Subindo req7

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+const Category = (sequelize, DataTypes) => {
+  const Category = sequelize.define('Category', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING,
+  },
+  {
+    underscored: true,
+    timestamps: false
+  });
+
+  return Category;
+};
+
+module.exports = Category;
